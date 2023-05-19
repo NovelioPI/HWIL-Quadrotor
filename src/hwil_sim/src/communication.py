e41bfa600a20279952ec3d628e2af0911474bd52
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2023-05-19 19:29:44 +0700

    add debug and improving algorithm

--- src/hwil_sim/src/communication.py
+++ src/hwil_sim/src/communication.py
@@ -4,27 +4,22 @@ import rospy
 import proto.hwil_pb2 as hwil_pb2
 from serial import Serial
 from time import sleep
+import codecs
 
 # message format: STX + message size (2 bytes) + message + checksum
 
 STX = b'\xFE'
 
 class Communication:
-    def __init__(self, port = '/dev/ttyACM0', baudrate = 115200):
-        while(True):
-            try:
-                self._serial = Serial(port, baudrate)
-                rospy.loginfo("Connected to " + port)
-                break
-            except:
-                pass
+    def __init__(self, port = '/dev/ttyACM0', baudrate = 9600):
+        self._serial = Serial(port, baudrate)
 
     def read(self):
         buffer = b''
         checksum = 0
         message = hwil_pb2.msg()
-        while True:
-            data = self._serial.read(1)
+        while self._serial.in_waiting > 3:
+            data = self._serial.read()
             if data == STX:
                 message_size = self._serial.read(1)[0] << 8 | self._serial.read(1)[0]
                 for i in range(message_size):
@@ -34,25 +29,35 @@ class Communication:
                 if checksum == self._serial.read(1)[0]:
                     message.ParseFromString(buffer)
                     return message
+                else:
+                    rospy.logerr("Checksum error")
+                    return None
+        # rospy.logerr("Message too short")
+        return None
 
     def write(self, message):
         buffer = message.SerializeToString()
-        checksum = 0
-        self._serial.write(STX)
-        self._serial.write((len(buffer) >> 8).to_bytes(1, 'big'))
-        self._serial.write((len(buffer) & 0xFF).to_bytes(1, 'big'))
-        for i in range(len(buffer)):
-            self._serial.write(buffer[i:i+1])
-            checksum ^= buffer[i]
-        self._serial.write(checksum.to_bytes(1, 'big'))
+        message_length = len(buffer)
 
-if __name__ == "__main__":
-    com = Communication(baudrate=9600)
-    hwil = hwil_pb2.msg()
-    while True:
-        hwil.sensors.sonar.distance = 122345
-        hwil.system_state = 1
-        com.write(hwil)
-        sleep(1)
+        self._serial.write(STX)
+        self._serial.write((message_length >> 8).to_bytes(1, 'big'))
+        self._serial.write((message_length & 0xFF).to_bytes(1, 'big'))
+        self._serial.write(buffer)
+
+        checksum = 0x00
+        for i in range(message_length):
+            checksum ^= buffer[i]
+        cb = checksum.to_bytes(1, 'big')
+        self._serial.write(cb)
+        self._serial.flush()
+
+# if __name__ == "__main__":
+#     com = Communication(baudrate=9600)
+#     hwil = hwil_pb2.msg()
+#     while True:
+#         hwil.sensors.sonar.distance = 122345
+#         hwil.system_state = 1
+#         com.write(hwil)
+#         sleep(1)
 
             
